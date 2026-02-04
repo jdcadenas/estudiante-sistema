35a2ddc660448bdfada67e7b8333183861b46584
José Cadenas <jdcadenas@gmail.com>
2026-02-03 21:58:45 -0400

    Add database configuration for production and local development in settings.py

--- asistencia_escolar/settings.py
+++ asistencia_escolar/settings.py
@@ -15,6 +15,9 @@ import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
+
+load_dotenv()  # Para desarrollo local
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,23 +80,27 @@ WSGI_APPLICATION = 'asistencia_escolar.wsgi.application'
 
 
 # Database
+# DATABASE CONFIGURATION
+DATABASE_URL = os.environ.get('DATABASE_URL')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-if os.environ.get('RENDER'):
+if DATABASE_URL:
+    # Producción (Render) - PostgreSQL
     DATABASES = {
-        'default': dj_database_url.config(  # Configure database using dj_database_url
-            default=os.environ.get('DATABASE_URL'),
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
             conn_max_age=600,
             conn_health_checks=True,
+            ssl_require=True  # Importante para Render
         )
     }
 else:
+    # Desarrollo local - SQLite
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 

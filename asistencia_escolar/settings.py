1ee67dffbf47de849cf77189f57fc11e6f6c5762
José Cadenas <jdcadenas@gmail.com>
2026-02-03 21:14:03 -0400

    Refactor code structure for improved readability and maintainability

--- asistencia_escolar/settings.py
+++ asistencia_escolar/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,14 +78,21 @@ WSGI_APPLICATION = 'asistencia_escolar.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': dj_database_url.config(  # Configure database using dj_database_url
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
-        conn_max_age=600
-    )
-}
-
+if os.environ.get('RENDER'):
+    DATABASES = {
+        'default': dj_database_url.config(  # Configure database using dj_database_url
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
--- build.sh
+++ build.sh
@@ -11,3 +11,5 @@ python manage.py migrate
 # Collect static files
 python manage.py collectstatic --no-input
 
+python manage.py migrate
+

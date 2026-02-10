d66bb88dcf345c417f4b647c6185e0c118fadea4
José Cadenas <jdcadenas@gmail.com>
2026-02-09 22:52:44 -0400

    adaptación del setting

--- asistencia_escolar/settings.py
+++ asistencia_escolar/settings.py
@@ -94,13 +94,13 @@ if DATABASE_URL:
         )
     }
 else:
-    # Desarrollo local - SQLite
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
